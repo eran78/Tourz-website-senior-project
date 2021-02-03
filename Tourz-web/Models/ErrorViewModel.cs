d1fc1c3bdbd334475fb78e79cb5f9ba785dcd16b
Eran <110078@st-maartenscollege.nl>
2021-02-03 14:58:41 +0100

    initial

---
+++ Tourz-web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tourz_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
