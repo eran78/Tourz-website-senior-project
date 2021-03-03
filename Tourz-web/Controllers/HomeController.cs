f26dedd0437374a883fdf1c5e777c16f5c70ca01
Remy v.d. Bosch <110032@st-maartenscollege.nl>
2021-03-03 15:29:13 +0100

    Contact form

--- Tourz-web/Controllers/HomeController.cs
+++ Tourz-web/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace Tourz_web.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
