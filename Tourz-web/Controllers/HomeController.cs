edbc63622ab33d967f0cb2283a74e3b8941e8643
eran78 <110078@st-maartenscollege.nl>
2021-03-30 13:47:21 +0200

    Connected website to needed database table

--- Tourz-web/Controllers/HomeController.cs
+++ Tourz-web/Controllers/HomeController.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Tourz_web.Models;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace Tourz_web.Controllers
 {
     public class HomeController : Controller
 
     {
-        string connectionString = "Server=172.16.160.21;Port=3306;Database=110078;Uid=110078;Pwd=nsRoUSEC;";
-
         private readonly ILogger<HomeController> _logger;
         public HomeController(ILogger<HomeController> logger)
         {
@@ -21,8 +21,34 @@ namespace Tourz_web.Controllers
         }
 
         public IActionResult Index()
-        {            
-            return View();
+        {
+            var names = GetNames();
+            
+            return View(names);
+        }
+        public List<string> GetNames()
+        {
+            string connectionString = "Server=172.16.160.21;Port=3306;Database=110078;Uid=110078;Pwd=nsRoUSEC;";
+
+            List<string> names = new List<string>();
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("select * from tourz website events", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string Name = reader["Name"].ToString();
+
+                        names.Add(Name);
+                    }
+                }
+            }
+            return names;
         }
 
         public IActionResult Privacy()
@@ -49,6 +75,8 @@ namespace Tourz_web.Controllers
         {
             return View();
         }
+
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
