f7d9ee8b4dcafcd1e7f2e058969cdabc5b87d34b
109750 <58980206+109750@users.noreply.github.com>
2021-04-01 16:10:33 +0200

    sync

--- Tourz-web/Controllers/HomeController.cs
+++ Tourz-web/Controllers/HomeController.cs
@@ -15,7 +15,8 @@ namespace Tourz_web.Controllers
 
     {
         private readonly ILogger<HomeController> _logger;
-        private string connectionString = "Server=172.16.160.21;Port=3306;Database=110078;Uid=110078;Pwd=nsRoUSEC;";
+        private string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110078;Uid=110078;Pwd=nsRoUSEC;";
+        //private string connectionString = "Server=172.16.160.21;Port=3306;Database=110078;Uid=110078;Pwd=nsRoUSEC;";
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -29,7 +30,7 @@ namespace Tourz_web.Controllers
         }
         public List<string> GetNames()
         {
-            string connectionString = "Server=172.16.160.21;Port=3306;Database=110078;Uid=110078;Pwd=nsRoUSEC;";
+         
 
             List<string> names = new List<string>();
 
