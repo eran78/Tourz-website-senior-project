d9c50026693172cecacf51f31564de59e819694f
Eran Hyman <110078@st-maartenscollege.nl>
2021-04-01 15:17:52 +0200

    (failed) attempt to show database table on site

---
+++ Tourz-web/Controllers/Database/Names.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tourz_web.Controllers.Database
+{
+    public class Names
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
